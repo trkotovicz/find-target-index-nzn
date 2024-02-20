e2766621d9fb4e77d83af15f8af0fdc84026300d
Thais Kotovicz <thais_kotovicz@hotmail.com>
2024-02-19 21:22:17 -0300

    feat: Final implementation of findTargetIndex using binary search

--- findTargetIndex.js
+++ findTargetIndex.js
@@ -1,3 +1,14 @@
-const findTargetIndex = (nums, target) => nums.indexOf(target);
+const findTargetIndex = (nums, target) => {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    const middle = Math.floor((left + right) / 2);
+    if (nums[middle] === target) return middle;
+    else if (nums[middle] < target) left = middle + 1;
+    else right = middle - 1;
+  }
+  return -1;
+}
 
 module.exports = findTargetIndex;
