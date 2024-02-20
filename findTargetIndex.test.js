010f0911de1844e48afaccc8c959008df5a5b471
Thais Kotovicz <thais_kotovicz@hotmail.com>
2024-02-19 21:19:57 -0300

    test: Add more tests for findTargetIndex

--- findTargetIndex.test.js
+++ findTargetIndex.test.js
@@ -8,3 +8,21 @@ it('Should return the target index if exists in the array', () => {
   const result = findTargetIndex(nums, target);
   expect(result).toBe(expected);
 });
+
+it('Should return -1 if the target does not exists in the array', () => {
+  const nums = [-1, 0, 3, 5, 9, 12];
+  const target = 2;
+  const expected = -1;
+
+  const result = findTargetIndex(nums, target);
+  expect(result).toEqual(expected);
+});
+
+it('Should return -1 if array is empty', () => {
+  const nums = [];
+  const target = 5;
+  const expected = -1;
+
+  const result = findTargetIndex(nums, target);
+  expect(result).toEqual(expected);
+});
